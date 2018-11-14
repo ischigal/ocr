b14e9c4d2fd1ed534f4834c5b9bb22dd8b14ea59
SebastianPanny <sebastian.panny@outlook.com>
2018-11-14 09:51:14 +0100

    fixed some commas in output, slightly worse distinction between soup and main course :P

--- lunchprinter.py
+++ lunchprinter.py
@@ -111,17 +111,17 @@ img_crop2 = img.crop(area2)
 #language option needs installation of file in usr/share/tessseract/4.00/tessdata       --psm 6 is a command line argument for tesseract to order tables correctly (not always needed, checke every week)
 out = (pytesseract.image_to_string(img_crop, lang='deu', config='--psm 6'))
 
-Mon = re.sub(" +", " ", re.search('Montag((?s).*)Dienstag', out).group(1).replace("\n"," ").strip())
-Die = re.sub(" +", " ", re.search('Dienstag((?s).*)Mittwoch', out).group(1).replace("\n"," ").strip())
-Mit = re.sub(" +", " ", re.search('Mittwoch((?s).*)Donnerstag', out).group(1).replace("\n"," ").strip())
-Don = re.sub(" +", " ", re.search('Donnerstag((?s).*)Freitag', out).group(1).replace("\n"," ").strip())
-Fre = re.sub(" +", " ", re.search('Freitag((?s).*)', out).group(1).replace("\n"," ").strip())
+Mon = re.sub(" +", " ", re.search('Montag((?s).*)Dienstag', out).group(1).replace("\n"," ").strip().replace(" , ",", "))
+Die = re.sub(" +", " ", re.search('Dienstag((?s).*)Mittwoch', out).group(1).replace("\n"," ").strip().replace(" , ",", "))
+Mit = re.sub(" +", " ", re.search('Mittwoch((?s).*)Donnerstag', out).group(1).replace("\n"," ").strip().replace(" , ",", "))
+Don = re.sub(" +", " ", re.search('Donnerstag((?s).*)Freitag', out).group(1).replace("\n"," ").strip().replace(" , ",", "))
+Fre = re.sub(" +", " ", re.search('Freitag((?s).*)', out).group(1).replace("\n"," ").strip().replace(" , ",", "))
 
 out2 = (pytesseract.image_to_string(img_crop2, lang='deu',config='--psm 6'))
 
-MBurger = re.sub(" +", " ", re.search('Monatsburger:((?s).*)Wochenburger', out2).group(1).replace("\n", " ").strip())
-WBurger = re.sub(" +", " ", re.search('Wochenburger:((?s).*)Wochenaktion', out2).group(1).replace("\n", " ").strip())
-WAktion  = re.sub(" +", " ", re.search('Wochenaktion:((?s).*)', out2).group(1).replace("\n", " ").strip())  # stupid 9b can't write for shit  ; != :
+MBurger = re.sub(" +", " ", re.search('Monatsburger:((?s).*)Wochenburger', out2).group(1).replace("\n", " ").strip().replace(" , ",", "))
+WBurger = re.sub(" +", " ", re.search('Wochenburger:((?s).*)Wochenaktion', out2).group(1).replace("\n", " ").strip().replace(" , ",", "))
+WAktion  = re.sub(" +", " ", re.search('Wochenaktion:((?s).*)', out2).group(1).replace("\n", " ").strip().replace(" , ",", "))  # stupid 9b can't write for shit  ; != :
 
 daylist = [Mon,Die,Mit,Don,Fre]
 NeunB = np.ndarray((5,4),dtype=object)
@@ -144,7 +144,7 @@ Men = np.ndarray((5,3),dtype=object)
 for jnd in range(0,5):
 	if jnd != 4:
 		for i in range(0,3):
-			Men[jnd][i] = re.sub('(€ ?\d+\,\d{1,2})', "", df[0]['data'][jnd+2][i+1]['text'].replace("\r", ", "))
+			Men[jnd][i] = re.sub('(€ ?\d+\,\d{1,2})', "", df[0]['data'][jnd+2][i+1]['text'].replace("\r", " "))
 	else:
 		for i in range(0,3):
 			Men[jnd][i] = re.sub('(€ ?\d+\,\d{1,2})', "", df[2]['data'][1][i+1]['text'].replace("\r", ", "))
@@ -159,7 +159,7 @@ Tec = np.ndarray((5,3),dtype=object)
 
 for knd in range(0,5):
 	for i in range(0,3):
-		Tec[knd][i] = re.sub('(€ ?\d+\,\d{1,2})', "", re.sub(' *\((.*?)\)', "", df2[0]['data'][knd+2][i+1]['text'].replace("\r", ", ")))
+		Tec[knd][i] = re.sub('(€ ?\d+\,\d{1,2})', "", re.sub(' *\((.*?)\)', "", df2[0]['data'][knd+2][i+1]['text'].replace("\r", " ")))
 
 ###################################################################################################
 
